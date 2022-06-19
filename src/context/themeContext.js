063e16fe84c3f8bbc502708eee03d3bdea635192
Deepak Kamat <dkamat.001@gmail.com>
2022-06-19 11:39:06 +0530

    feat: add theme context with local storage

--- src/context/themeContext.js
+++ src/context/themeContext.js
@@ -1,7 +1,27 @@
-const { createContext } = require("react");
+const { createContext, useState, useEffect, useContext } = require("react");
 
 const themeContext = createContext({ theme: "light" });
 
 const themeProvider = ({ children }) => {
-  return <themeContext.provider value={{}}>{children}</themeContext.provider>;
+  const [theme, setTheme] = useState("light");
+  const changeTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    setTheme(localStorage.getItem("theme") ?? "light");
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <themeContext.provider value={{ theme, changeTheme }}>
+      {children}
+    </themeContext.provider>
+  );
 };
+
+const useTheme = () => useContext(themeContext);
+export { useTheme, themeProvider };
